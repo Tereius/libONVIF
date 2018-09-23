381908d11f737f160418908fee89a6b5ee1f5458
Björn Stresing <bjoern.stresing@gmx.de>
2018-09-23 17:00:35 +0200

    Android build wip, switched to snapshot version

--- conanfile.py
+++ conanfile.py
@@ -3,24 +3,23 @@
 
 
 import json, os
-from conans import ConanFile, CMake, tools
+from conans import ConanFile, CMake, tools, AutoToolsBuildEnvironment
 
 
 class LibonvifConan(ConanFile):
     jsonInfo = json.loads(tools.load("info.json"))
     name = jsonInfo["projectName"]
-    version = "%u.%u.%u" % (jsonInfo["version"]["major"], jsonInfo["version"]["minor"], jsonInfo["version"]["patch"])
+    version = "%u.%u.%u%s" % (jsonInfo["version"]["major"], jsonInfo["version"]["minor"], jsonInfo["version"]["patch"], "-SNAPSHOT" if jsonInfo["version"]["snapshot"] else "")
     license = jsonInfo["license"]
     url = jsonInfo["repository"]
     description = jsonInfo["projectDescription"]
     author = jsonInfo["vendor"]
     homepage = jsonInfo["repository"]
     requires = "Qt/[>=5.4]@tereius/stable"
-    settings = "os", "compiler", "build_type", "arch"
+    settings = "os", "compiler", "build_type", "arch", "os_build", "arch_build"
     options = {"shared": [True, False], "openssl": [True, False]}
-    default_options = "shared=True", "openssl=False"
+    default_options = "shared=True", "openssl=True", "OpenSSL:shared=True"
     generators = "cmake"
-    build_policy = "missing"
     exports = "info.json"
     exports_sources = "*"
 
@@ -28,19 +27,33 @@ class LibonvifConan(ConanFile):
         if self.options.openssl:
             self.requires("OpenSSL/1.1.0g@conan/stable")
 
-    def source(self):
-        # This small hack might be useful to guarantee proper /MT /MD linkage
-        # in MSVC if the packaged project doesn't have variables to set it
-        # properly
-        tools.replace_in_file("CMakeLists.txt", "### CONAN_BEACON ###",
-'''include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-conan_basic_setup()''')
+    def build_requirements(self):
+        if self.settings.os == 'Android':
+            self.build_requires("android-ndk/r17b@tereius/stable")
+            if self.settings.os_build == 'Windows':
+                self.build_requires("msys2/20161025@tereius/stable")
 
     def build(self):
-        cmake = CMake(self)
-        cmake.configure()
-        cmake.build()
-        cmake.install()
+        tools.replace_in_file(os.path.join(self.build_folder, "CMakeLists.txt"), "### CONAN_BEACON ###", 'include(%s)\n%s' % (os.path.join(self.install_folder, "conanbuildinfo.cmake").replace("\\", "/"), "conan_basic_setup()"), strict=False)
+        if self.settings.os == 'Android':
+            autotools = AutoToolsBuildEnvironment(self, win_bash=True)
+            environment = autotools.vars
+            #environment["CC"] = self.deps_env_info['android-ndk'].CC
+            #environment["CXX"] = self.deps_env_info['android-ndk'].CXX
+            while tools.environment_append(autotools.vars):
+                self.run("whereis cmake", win_bash=True)
+                cmake = CMake(self, generator="Unix Makefiles")
+                self.run('cmake "%s" %s -DCMAKE_C_COMPILER=%s -DCMAKE_CXX_COMPILER=%s' % (self.source_folder, cmake.command_line, tools.unix_path(self.deps_env_info['android-ndk'].CC), tools.unix_path(self.deps_env_info['android-ndk'].CXX)), win_bash=True)
+                #cmake.configure(defs = {"CMAKE_MAKE_PROGRAM": tools.unix_path(os.path.join(self.deps_env_info['msys2'].MSYS_BIN, "make.exe")), "CMAKE_C_COMPILER": tools.unix_path(self.deps_env_info['android-ndk'].CC), "CMAKE_CXX_COMPILER": tools.unix_path(self.deps_env_info['android-ndk'].CXX)})
+                #cmake.build()
+                #cmake.install()
+        else:
+            cmake = CMake(self)
+            cmake.configure()
+            cmake.build()
+            cmake.install()
 
     def package_info(self):
         self.cpp_info.builddirs = ['cmake']
+        if self.options.openssl:
+            self.cpp_info.defines = ['WITH_OPENSSL']
